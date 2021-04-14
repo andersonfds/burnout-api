952dfcb51c1dc97da388f4f873c0692be07e4ca6
andersonfds <andersonlivegja.as@gmail.com>
2021-04-14 00:52:48 -0300

    configuração do bootstrap de inicialização, variaveis de ambiente e swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,41 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from '@src/app.module';
+import * as dotnev from 'dotenv';
 
+/**
+ * This is the application bootstrap run pipeline
+ */
 async function bootstrap() {
+  // Enabling environment variables
+  dotnev.config();
+
+  // Getting environment variables
+  const {
+    APP_NAME,
+    APP_DESCRIPTION,
+    APP_VERSION,
+    APP_SERVE,
+    SWAGGER_ENDPOINT
+  } = process.env;
+  
+  // Creating a nest app
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Adding swagger options
+  const config = new DocumentBuilder()
+    .setTitle(APP_NAME)
+    .setDescription(APP_DESCRIPTION)
+    .setVersion(APP_VERSION)
+    .setTitle(APP_NAME)
+    .addBearerAuth()
+    .build();
+
+  // Configuring Swagger
+  const doc = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(SWAGGER_ENDPOINT, app, doc);
+
+  // Starting the app
+  await app.listen(APP_SERVE);
 }
 bootstrap();
