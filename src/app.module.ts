00a51565889ec945eba0b8404ecaf7367e796b78
andersonfds <andersonlivegja.as@gmail.com>
2021-04-18 19:55:46 -0300

    atualização no ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from '@src/resources/auth/auth.module';
 import { UserModule } from './resources/user/user.module';
+import { ActivityModule } from './resources/activity/activity.module';
 
 
 @Module({
@@ -14,7 +15,7 @@ import { UserModule } from './resources/user/user.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (config: ConfigService) => ({
-        type: 'mysql',
+        type: config.get<any>('DB_TYPE'),
         host: config.get<string>('DB_HOST'),
         port: config.get<number>('DB_PORT'),
         name: 'default',
@@ -27,6 +28,7 @@ import { UserModule } from './resources/user/user.module';
     }),
     UserModule,
     AuthModule,
+    ActivityModule,
   ],
   providers: [TypeOrmModule],
   exports: [],
