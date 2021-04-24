946d038954de05c3fb084078ce7bd7da9ef28ebc
andersonfds <andersonlivegja.as@gmail.com>
2021-04-24 10:02:58 -0300

    adicionado documentação

--- src/resources/step/dto/response-step.dto.ts
+++ src/resources/step/dto/response-step.dto.ts
@@ -1,13 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { Exclude, Expose } from "class-transformer";
 
 @Exclude()
 export class ResponseStepDto {
+    @ApiProperty()
     @Expose()
     id: string;
 
+    @ApiProperty()
     @Expose()
     type: string;
-
+    
+    @ApiProperty()
     @Expose()
     content: any;
 }
