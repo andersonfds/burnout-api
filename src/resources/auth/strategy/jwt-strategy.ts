0a57ff1d794464f285324b41e151d0148660f397
andersonfds <andersonlivegja.as@gmail.com>
2021-04-25 10:15:55 -0300

    ignorando expiração do token

--- src/resources/auth/strategy/jwt-strategy.ts
+++ src/resources/auth/strategy/jwt-strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     constructor(config: ConfigService) {
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-            ignoreExpiration: false,
+            ignoreExpiration: config.get<boolean>('APP_IGNORE_JWT_EXPIRATION') || true,
             secretOrKey: config.get<string>('APP_SECRET'),
         });
     }
